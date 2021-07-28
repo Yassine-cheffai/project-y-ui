694b2bf79750d4db86c92d78ca4765f4e67aed66
Yassine-cheffai <yacincheffai@yahoo.com>
2021-07-28 12:31:35 +0200

    prettier TS

--- src/components/twitter/tweetsTable.tsx
+++ src/components/twitter/tweetsTable.tsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import { DataGrid, GridColDef } from '@material-ui/data-grid';
+import React from "react";
+import { DataGrid, GridColDef } from "@material-ui/data-grid";
 
 type Series = {
-  name: string,
-  data: number[]
-}
+  name: string;
+  data: number[];
+};
 type ChartData = {
-  categories: string[],
-  series: Series,
-}
+  categories: string[];
+  series: Series;
+};
 type TweetsData = {
-  subject: string[],
-  polarity: number[],
-  tweet_text: string[],
-  id: number[],
-  status: string[],
-  created_at: string[],
-}
+  subject: string[];
+  polarity: number[];
+  tweet_text: string[];
+  id: number[];
+  status: string[];
+  created_at: string[];
+};
 type TwitterResult = {
-  chart_data: ChartData,
-  tweets_data: TweetsData,
-}
+  chart_data: ChartData;
+  tweets_data: TweetsData;
+};
 type Props = {
   twitterResult: TwitterResult | undefined;
-}
+};
 
 export const TweetsTable: React.FC<Props> = (props) => {
   const { twitterResult } = props;
@@ -31,53 +31,50 @@ export const TweetsTable: React.FC<Props> = (props) => {
   const columns: GridColDef[] = [
     //{ field: 'subject', headerName: 'Subject', width: 90 },
     //{
-      //field: 'polarity',
-      //headerName: 'Polarity',
-      //width: 150,
-      //editable: true,
+    //field: 'polarity',
+    //headerName: 'Polarity',
+    //width: 150,
+    //editable: true,
     //},
     {
-      field: 'tweet_text',
-      headerName: 'Tweet Text',
+      field: "tweet_text",
+      headerName: "Tweet Text",
       width: 500,
       editable: true,
     },
     //{
-      //field: 'id',
-      //headerName: 'Tweet ID',
-      //width: 110,
+    //field: 'id',
+    //headerName: 'Tweet ID',
+    //width: 110,
     //},
     {
-      field: 'status',
-      headerName: 'Status',
-      type: 'number',
+      field: "status",
+      headerName: "Status",
+      type: "number",
       width: 200,
     },
     {
-      field: 'created_at',
-      headerName: 'Created At',
-      type: 'number',
+      field: "created_at",
+      headerName: "Created At",
+      type: "number",
       width: 200,
     },
   ];
 
   let tweetsRows = [];
-  const rowsNumber = twitterResult?.tweets_data.subject.length || 0
+  const rowsNumber = twitterResult?.tweets_data.subject.length || 0;
   for (let i = 0; i < rowsNumber; i++) {
-    tweetsRows.push(
-      {
-        subject: twitterResult?.tweets_data.subject[i],
-        polarity: twitterResult?.tweets_data.polarity[i],
-        tweet_text: twitterResult?.tweets_data.tweet_text[i],
-        id: twitterResult?.tweets_data.id[i],
-        status: twitterResult?.tweets_data.status[i],
-        created_at: twitterResult?.tweets_data.created_at[i],
-      }
-
-    )
+    tweetsRows.push({
+      subject: twitterResult?.tweets_data.subject[i],
+      polarity: twitterResult?.tweets_data.polarity[i],
+      tweet_text: twitterResult?.tweets_data.tweet_text[i],
+      id: twitterResult?.tweets_data.id[i],
+      status: twitterResult?.tweets_data.status[i],
+      created_at: twitterResult?.tweets_data.created_at[i],
+    });
   }
   return (
-    <div style={{ height: 400, width: '100%' }}>
+    <div style={{ height: 400, width: "100%" }}>
       <DataGrid
         rows={tweetsRows}
         columns={columns}
@@ -86,5 +83,5 @@ export const TweetsTable: React.FC<Props> = (props) => {
         disableSelectionOnClick
       />
     </div>
-  )
-}
+  );
+};

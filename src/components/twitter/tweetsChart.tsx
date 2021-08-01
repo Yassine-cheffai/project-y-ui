a129cef8713a4fcb23d4cf92f9a69e1458c0e65b
Yassine-cheffai <yacincheffai@yahoo.com>
2021-08-01 22:54:51 +0200

    formatting

--- src/components/twitter/tweetsChart.tsx
+++ src/components/twitter/tweetsChart.tsx
@@ -1,64 +1,59 @@
-import React from 'react';
+import React from "react";
 //import TwitterResult from './twitter';
-import Highcharts from 'highcharts'
-import HighchartsReact from 'highcharts-react-official'
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
 
 type Series = {
-  name: string,
-  data: number[]
-}
+  name: string;
+  data: number[];
+};
 type ChartData = {
-  categories: string[],
-  series: Series,
-}
+  categories: string[];
+  series: Series;
+};
 type TweetsData = {
-  subject: string[],
-  polarity: number[],
-  tweet_text: string[],
-  id: number[],
-  status: string[],
-  created_at: string[],
-}
+  subject: string[];
+  polarity: number[];
+  tweet_text: string[];
+  id: number[];
+  status: string[];
+  created_at: string[];
+};
 type TwitterResult = {
-  chart_data: ChartData,
-  tweets_data: TweetsData,
-}
+  chart_data: ChartData;
+  tweets_data: TweetsData;
+};
 type Props = {
   twitterResult: TwitterResult | undefined;
-}
+};
 
 export const TweetsChart: React.FC<Props> = (props) => {
   const { twitterResult } = props;
   let options = {
-    title : {
-      text : "Twitter Text"
+    title: {
+      text: "Twitter Text",
     },
     //chart: {
-      //type: "column"
+    //type: "column"
     //},
     xAxis: {
-      categories : twitterResult?.chart_data.categories,
+      categories: twitterResult?.chart_data.categories,
     },
     yAxis: {
-      title: "feedbacks"
+      title: "feedbacks",
     },
-    series : twitterResult?.chart_data.series
-  }
-  
-   return (
-     <div>
-       <h3>Twitter Chart Component</h3>
-       { twitterResult?.chart_data.categories.map((categorie) => (
-         <li key={categorie}>{ categorie }</li>
-       ))
-       }
-       <div>
-         <HighchartsReact
-           highcharts={ Highcharts }
-           options={ options }
-         />
-       </div>
-    </div>
-   )
-  }
+    series: twitterResult?.chart_data.series,
+  };
 
+  return (
+    <div>
+      <h3>Twitter Chart Component</h3>
+      {twitterResult?.chart_data.categories.map((categorie) => (
+        <li key={categorie}>{categorie}</li>
+      ))}
+      <div>
+        <HighchartsReact highcharts={Highcharts} options={options} />
+      </div>
+    </div>
+  );
+};
